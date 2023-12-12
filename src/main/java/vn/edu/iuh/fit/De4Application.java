aa3f1845c82df068c1139f87d724b1ddbcd4b762
ThuyTien2111 <vanbac21112k2@gmail.com>
2023-12-12 13:48:28 +0700

    done

---
+++ src/main/java/vn/edu/iuh/fit/De4Application.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.de4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class De4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(De4Application.class, args);
+    }
+
+}
---
+++ src/test/java/vn/edu/iuh/fit/de4/De4ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.de4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class De4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
